31c491006884b0322ccdeb394fd89c5e15d3e859
Sukanto Mukherjee <m.sukanto@gmail.com>
2020-07-12 11:39:40 +0530

    Python package: adding files for package

---
+++ normbinom/README.md
@@ -0,0 +1,8 @@
+# normbinom Package
+
+### This is a simple Python package to look at [Gaussian](https://en.wikipedia.org/wiki/Normal_distribution) and [Binomial](https://en.wikipedia.org/wiki/Binomial_distribution)
+### probability distributions.
+
+## Installation
+
+pip install normbinom
---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='normbinom',
+      version='0.1',
+      description='Gaussian & Binomial distributions',
+      packages=['normbinom'],
+      author = 'Sukanto Mukherjee',
+      author_email = 'readrunner@protonmail.com',
+      zip_safe=False)
